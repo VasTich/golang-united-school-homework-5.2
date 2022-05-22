25932e0ad25a0c672fb9e68302064ef152fb222c
alopatkin <alxlopatkin@gmail.com>
2022-05-22 21:49:11 +0300

    add solution

--- cache.go
+++ cache.go
@@ -3,21 +3,30 @@ package cache
 import "time"
 
 type Cache struct {
+	dict map[string]string
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{dict: make(map[string]string)}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	value, ok := c.dict[key]
+	return value, ok
 }
 
-func (receiver) Put(key, value string) {
+func (c Cache) Put(key, value string) {
+	c.dict[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	var keys []string
+	for k, _ := range c.dict {
+		keys = append(keys, k)
+	}
+	
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
 }
