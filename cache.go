5a29a231cc9ff2344ada028e24478d8cfbe5b0ab
alopatkin <alxlopatkin@gmail.com>
2022-05-22 23:13:35 +0300

    add expiration time

--- cache.go
+++ cache.go
@@ -2,21 +2,47 @@ package cache
 
 import "time"
 
+type DeadlineValue struct {
+	value    string
+	expired  bool
+	deadline time.Time
+}
+
+func NewNoDeadlineValue(val string) DeadlineValue {
+	return DeadlineValue{value: val, expired: false, deadline: time.Now()}
+}
+
+func NewDeadLineValue(val string, expiredTime time.Time) DeadlineValue {
+	return DeadlineValue{value: val, expired: true, deadline: expiredTime}
+}
+
 type Cache struct {
-	dict map[string]string
+	dict  map[string]DeadlineValue
+	timer *time.Timer
 }
 
 func NewCache() Cache {
-	return Cache{dict: make(map[string]string)}
+	var cache Cache
+	cache.dict = make(map[string]DeadlineValue)
+	cache.timer = time.AfterFunc(time.Second, func() {
+		expiryTime := time.Now()
+		for k, v := range cache.dict {
+			if v.expired && v.deadline.After(expiryTime) {
+				delete(cache.dict, k)
+			}
+		}
+	})
+
+	return cache
 }
 
 func (c Cache) Get(key string) (string, bool) {
 	value, ok := c.dict[key]
-	return value, ok
+	return value.value, ok
 }
 
 func (c Cache) Put(key, value string) {
-	c.dict[key] = value
+	c.dict[key] = NewNoDeadlineValue(value)
 }
 
 func (c Cache) Keys() []string {
@@ -24,9 +50,10 @@ func (c Cache) Keys() []string {
 	for k, _ := range c.dict {
 		keys = append(keys, k)
 	}
-	
+
 	return keys
 }
 
 func (c Cache) PutTill(key, value string, deadline time.Time) {
+	c.dict[key] = NewDeadLineValue(value, deadline)
 }
